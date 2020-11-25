3355a346311dad10a832103a6e591be09024ccff
LAPTOP-U003GCDR\anguy <nducanh2311@gmail.com>
2020-11-25 16:14:41 +0700

    Fetch first game API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loadGames } from "./actions/gamesAction";
 
 export default function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadGames());
+  });
   return (
     <div className="App">
       <h1>Hello Ignite</h1>
--- src/api.js
+++ src/api.js
@@ -31,5 +31,5 @@ const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 // Popular Games
-const popular_games = `game?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
 export const popularGamesURL = () => `${base_url}${popular_games}`;
